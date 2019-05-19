30335686a6dbb09eb6d6956f2c3464db921e8599
imkaladdin <mik31@mail.com>
2019-05-19 20:29:46 +0100

    18 flickr api in vanilla javascript ajax (in jsonP really)

---
+++ 19.js
@@ -0,0 +1,10 @@
+	function handleTheResponse(data) {
+		let jsp = document.getElementById("response").textContent = JSON.stringify(data,null,2);
+	}
+
+	const kiKscript = document.querySelector(`script[src*="kiK.js"]`);
+	const kiKparent = kiKscript.parentNode;
+	
+	const zscript = document.createElement("script");
+	zscript.src = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=handleTheResponse&tags=london&tagmode=any&format=json";
+	kiKparent.insertBefore(zscript, kiKscript);
