031dddbc2d087f10c99d450b991118b094560023
imkaladdin <mik31@mail.com>
2019-05-22 19:56:56 +0100

    22 IK solution to api jsonp but only if you set the url query string before and it cannot be changed

--- 19.js
+++ 19.js
@@ -8,3 +8,5 @@
 	const zscript = document.createElement("script");
 	zscript.src = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=handleTheResponse&tags=london&tagmode=any&format=json";
 	kiKparent.insertBefore(zscript, kiKscript);
+
+/*COLLECTED FROM ONLINE, AND THOUGHT I COULD STORE IT IN A .JS FILE BUT STILL NO GOOD AS I CANNOT SEEM TO USE THE DATA*/
