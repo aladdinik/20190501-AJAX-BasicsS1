f7b7b9b5c7b2d393f2d95691dae63f68337e7c78
imkaladdin <mik31@mail.com>
2019-06-11 11:41:00 +0100

    recap 20190611

--- 19.js
+++ 19.js
@@ -4,7 +4,7 @@
 
 	const kiKscript = document.querySelector(`script[src*="kiK.js"]`);
 	const kiKparent = kiKscript.parentNode;
-	
+
 	const zscript = document.createElement("script");
 	zscript.src = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=handleTheResponse&tags=london&tagmode=any&format=json";
 	kiKparent.insertBefore(zscript, kiKscript);
